61cafd02e2ea90761a12454a6b44f963efd684f9
Mansi <kshirsagarmansi1302@gmail.com>
2026-07-25 00:31:17 +0530

    Initial commit

---
+++ Inte.java
@@ -0,0 +1,20 @@
+public class Inte {
+    public static void main(String[] args) {
+
+        //integer
+        byte a = 5;
+        short b = 10;
+        int c = 123;
+        long d = 2434;
+
+        //floate Real no
+
+        float f=9.08f;
+        double g=33.240979128964;
+
+
+
+
+        System.out.println("Integer values => " + a + ", " + b + ", " + c + ", " + d);
+    }
+}
